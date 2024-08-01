0adbda233daa0671d50542da4f36b8f92228e706
nayeon <20192279@sungshin.ac.kr>
2024-08-01 09:01:39 +0900

    add video

--- src/components/modals/SecModal.jsx
+++ src/components/modals/SecModal.jsx
@@ -18,7 +18,23 @@ function SecModal(props) {
           </div>
         </div>
         <div className="about2-content dogica">
-          <p className="center sixt">2023.4-6 <br /> graphic design : Kota Higrakawa <br /> motion graphic : Gayoung Rhee / 3D : Yushi Ouchida</p>
+          <p className="center sixt">
+            2023.4-6 <br />
+            graphic design : Kota Higrakawa <br />
+            motion graphic : Gayoung Rhee / 3D : Yushi Ouchida
+          </p>
+          {/* 비디오 링크 추가 부분 */}
+          <div className="video-container">
+            <iframe
+              src="https://www.youtube.com/embed/VIDEO_ID"  // 여기에 실제 비디오 ID를 삽입하세요.
+              title="Video description"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              width="100%"
+              height="315"
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
